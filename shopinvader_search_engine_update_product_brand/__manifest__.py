ac85c220b0ee16b0350ee820dc247fd2d43291f1
Daniel Sadamo <daniel.sadamo@kmee.com.br>
2025-07-16 11:57:30 -0300

    [MIG] shopinvader_search_engine_update_product_brand: Migration to 17.0

--- shopinvader_search_engine_update_product_brand/__manifest__.py
+++ shopinvader_search_engine_update_product_brand/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Shopinvader Search Engine Update Product Brand",
     "summary": """
         Shopinvader: Mark brand and product bindings to export on product brand update""",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "website": "https://github.com/shopinvader/odoo-shopinvader",
