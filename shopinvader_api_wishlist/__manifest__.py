eda1b8cfbbddd4fa8aa3ff9797550a773f760f94
Daniel Sadamo <daniel.sadamo@kmee.com.br>
2025-07-16 11:57:27 -0300

    [MIG] shopinvader_api_wishlist: Migration to 17.0

--- shopinvader_api_wishlist/__manifest__.py
+++ shopinvader_api_wishlist/__manifest__.py
@@ -6,7 +6,7 @@
     "name": "Shopinvader Wishlist",
     "summary": """
         Handle shop wishlist""",
-    "version": "16.0.1.0.2",
+    "version": "17.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV, Camptocamp,Odoo Community Association (OCA)",
     "website": "https://github.com/shopinvader/odoo-shopinvader",
