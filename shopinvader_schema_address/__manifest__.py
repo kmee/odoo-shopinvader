767a1d21783cf85ddce9b064ec569c296157c7ff
Daniel Sadamo <daniel.sadamo@kmee.com.br>
2025-07-16 11:57:30 -0300

    [MIG] shopinvader_schema_address: Migration to 17.0

--- shopinvader_schema_address/__manifest__.py
+++ shopinvader_schema_address/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Shopinvader Schema Address",
     "summary": """
         Adds shchema address: address invoicing_address delivery_address""",
-    "version": "16.0.1.3.2",
+    "version": "17.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV",
     "website": "https://github.com/shopinvader/odoo-shopinvader",
