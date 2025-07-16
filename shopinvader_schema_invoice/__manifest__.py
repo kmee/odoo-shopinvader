7cbbbcc0359bee286abb9985710edb5daf680ad2
Daniel Sadamo <daniel.sadamo@kmee.com.br>
2025-07-16 11:57:30 -0300

    [MIG] shopinvader_schema_invoice: Migration to 17.0

--- shopinvader_schema_invoice/__manifest__.py
+++ shopinvader_schema_invoice/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Shopinvader schema invoice",
     "summary": "Add schema for invoices",
-    "version": "16.0.1.0.1",
+    "version": "17.0.1.0.0",
     "development_status": "Alpha",
     "website": "https://github.com/shopinvader/odoo-shopinvader",
     "author": " Camptocamp",
