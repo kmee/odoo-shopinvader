fabb1ccf4a0f96b97a4dd36b85466c4ad4d51193
Daniel Sadamo <daniel.sadamo@kmee.com.br>
2025-07-16 11:57:30 -0300

    [MIG] shopinvader_v2_app_demo: Migration to 17.0

--- shopinvader_v2_app_demo/__manifest__.py
+++ shopinvader_v2_app_demo/__manifest__.py
@@ -7,7 +7,7 @@
         Adds a demo application on fastapi endpoint for a
         demo shopinvader application that takes every router
         """,
-    "version": "16.0.1.0.1",
+    "version": "17.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV",
     "website": "https://github.com/shopinvader/odoo-shopinvader",
