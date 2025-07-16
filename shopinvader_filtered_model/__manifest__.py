cf3c250cc74bcd5f6c553f5be53c82b0baf56cb4
Daniel Sadamo <daniel.sadamo@kmee.com.br>
2025-07-16 11:57:28 -0300

    [MIG] shopinvader_filtered_model: Migration to 17.0

--- shopinvader_filtered_model/__manifest__.py
+++ shopinvader_filtered_model/__manifest__.py
@@ -6,7 +6,7 @@
 {
     "name": "Shopinvader filtered model",
     "summary": "Helper for exposing model easily",
-    "version": "16.0.1.1.1",
+    "version": "17.0.1.0.0",
     "development_status": "Alpha",
     "category": "Uncategorized",
     "website": "https://github.com/shopinvader/odoo-shopinvader",
