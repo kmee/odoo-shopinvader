3c097e99c037579a4b5e295c6507e6a9ec4b9f05
Daniel Sadamo <daniel.sadamo@kmee.com.br>
2025-07-16 11:57:30 -0300

    [MIG] shopinvader_search_engine_update_product_template_multi_link: Migration to 17.0

--- shopinvader_search_engine_update_product_template_multi_link/__manifest__.py
+++ shopinvader_search_engine_update_product_template_multi_link/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Shopinvader Search Engine Update Product Template Multi Link",
     "summary": """
         Mark bindings to recompute when a link is added, deleted or modified""",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV",
     "website": "https://github.com/shopinvader/odoo-shopinvader",
