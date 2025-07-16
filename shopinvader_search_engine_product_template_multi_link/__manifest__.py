2bc896a0e2c12aaf2bfd3f09f0ec7a17f28d2dd8
Daniel Sadamo <daniel.sadamo@kmee.com.br>
2025-07-16 11:57:30 -0300

    [MIG] shopinvader_search_engine_product_template_multi_link: Migration to 17.0

--- shopinvader_search_engine_product_template_multi_link/__manifest__.py
+++ shopinvader_search_engine_product_template_multi_link/__manifest__.py
@@ -7,7 +7,7 @@
 {
     "name": "Shopinvader Product Link",
     "summary": "Shopinvader Product Link",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "category": "e-commerce",
     "website": "https://github.com/shopinvader/odoo-shopinvader",
     "author": "Akretion,GRAP,Camptocamp,ACSONE SA/NV",
