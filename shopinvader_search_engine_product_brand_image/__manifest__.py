bba023302c42cb6373461013ad1a0e5d18524037
Daniel Sadamo <daniel.sadamo@kmee.com.br>
2025-07-16 11:57:30 -0300

    [MIG] shopinvader_search_engine_product_brand_image: Migration to 17.0

--- shopinvader_search_engine_product_brand_image/__manifest__.py
+++ shopinvader_search_engine_product_brand_image/__manifest__.py
@@ -6,7 +6,7 @@
 {
     "name": "Shopinvader Search Engine Product Brand Image",
     "summary": "Shopinvader Search Engine product Brand Image",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "category": "Shopinvader",
     "website": "https://github.com/shopinvader/odoo-shopinvader",
     "author": " Akretion,ACSONE SA/NV",
