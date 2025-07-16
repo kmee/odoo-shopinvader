0eaefeccdda682fd95519d54d42bfbd7481935da
Daniel Sadamo <daniel.sadamo@kmee.com.br>
2025-07-16 11:57:29 -0300

    [MIG] shopinvader_product_url: Migration to 17.0

--- shopinvader_product_url/__manifest__.py
+++ shopinvader_product_url/__manifest__.py
@@ -6,7 +6,7 @@
 {
     "name": "Shopinvader product url",
     "summary": "Generate url for product and category",
-    "version": "16.0.1.0.1",
+    "version": "17.0.1.0.0",
     "development_status": "Alpha",
     "category": "Shopinvader",
     "website": "https://github.com/shopinvader/odoo-shopinvader",
