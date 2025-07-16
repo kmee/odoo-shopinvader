5efde69e9d70da65572e5a98b2cc222375fc15d4
Daniel Sadamo <daniel.sadamo@kmee.com.br>
2025-07-16 11:57:29 -0300

    [MIG] shopinvader_product_brand: Migration to 17.0

--- shopinvader_product_brand/__manifest__.py
+++ shopinvader_product_brand/__manifest__.py
@@ -6,7 +6,7 @@
 {
     "name": "Shopinvader Product Brand",
     "summary": "Shopinvader product Brand",
-    "version": "16.0.1.0.1",
+    "version": "17.0.1.0.0",
     "category": "Shopinvader",
     "website": "https://github.com/shopinvader/odoo-shopinvader",
     "author": " Akretion",
