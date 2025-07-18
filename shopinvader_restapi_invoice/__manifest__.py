6b56e0682cc09a9d61c44d0db5423cdc82ba1ee4
Daniel Sadamo <daniel.sadamo@kmee.com.br>
2025-07-18 13:40:09 -0300

    [MIG] shopinvader_restapi_invoice: Migration to 17.0

--- shopinvader_restapi_invoice/__manifest__.py
+++ shopinvader_restapi_invoice/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "ACSONE SA/NV",
     "website": "https://github.com/shopinvader/odoo-shopinvader",
     "category": "shopinvader",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "license": "AGPL-3",
     "depends": ["account", "shopinvader_restapi"],
     "data": [],
