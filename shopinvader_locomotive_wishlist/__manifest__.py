16228d383cf8ded0d7c41b9a0ba8370e072b5ff1
Daniel Sadamo <daniel.sadamo@kmee.com.br>
2025-07-25 18:38:25 -0300

    [MIG] shopinvader_locomotive_wishlist: migrate to v17.0.1.0.0 with installable=False

--- shopinvader_locomotive_wishlist/__manifest__.py
+++ shopinvader_locomotive_wishlist/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Shopinvader Locomotive Wishlist",
     "summary": "Synchronize wishlist details to Locomotive users record.",
-    "version": "14.0.1.0.2",
+    "version": "17.0.1.0.0",
     "category": "e-commerce",
     "website": "https://github.com/shopinvader/odoo-shopinvader",
     "author": "Camptocamp",
