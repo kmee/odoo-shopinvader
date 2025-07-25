815fc0838cdce923f46924906566e054bed2c9b6
Daniel Sadamo <daniel.sadamo@kmee.com.br>
2025-07-25 18:38:25 -0300

    [MIG] shopinvader_portal_mode: migrate to v17.0.1.0.0 with installable=False

--- shopinvader_portal_mode/__manifest__.py
+++ shopinvader_portal_mode/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Shopinvader portal mode",
     "Summary": "Enable portal-like features",
-    "version": "14.0.1.1.0",
+    "version": "17.0.1.0.0",
     "license": "AGPL-3",
     "author": "Camptocamp SA",
     "website": "https://github.com/shopinvader/odoo-shopinvader",
