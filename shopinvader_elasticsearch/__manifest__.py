527c99b869a34642d02eb6725eb56468ff9666b4
Daniel Sadamo <daniel.sadamo@kmee.com.br>
2025-07-25 18:38:26 -0300

    [MIG] shopinvader_elasticsearch: migrate to v17.0.1.0.0 with installable=False

--- shopinvader_elasticsearch/__manifest__.py
+++ shopinvader_elasticsearch/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Shopinvader Elasticsearch",
     "summary": """
         Shopinvader Elasticsearch Connector""",
-    "version": "14.0.3.0.1",
+    "version": "17.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV",
     "website": "https://github.com/shopinvader/odoo-shopinvader",
