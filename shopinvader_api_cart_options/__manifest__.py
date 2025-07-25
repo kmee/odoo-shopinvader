51c8a6ab9f0b816977f4a60be185f1114d848efd
Daniel Sadamo <daniel.sadamo@kmee.com.br>
2025-07-25 18:38:26 -0300

    [MIG] shopinvader_api_cart_options: migrate to v17.0.1.0.0 with installable=False

--- shopinvader_api_cart_options/__manifest__.py
+++ shopinvader_api_cart_options/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Shopinvader API Cart Options",
-    "version": "16.0.1.0.1",
+    "version": "17.0.1.0.0",
     "author": "Akretion, Odoo Community Association (OCA)",
     "summary": "Add product options to the cart API",
     "depends": ["shopinvader_api_cart"],
