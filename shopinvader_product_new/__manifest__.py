062cd3f796525172ddb46bba4991d2d6c35ac5a9
Daniel Sadamo <daniel.sadamo@kmee.com.br>
2025-07-25 18:38:26 -0300

    [MIG] shopinvader_product_new: migrate to v17.0.1.0.0 with installable=False

--- shopinvader_product_new/__manifest__.py
+++ shopinvader_product_new/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Shopinvader Product New",
     "summary": "Shopinvader product new",
-    "version": "14.0.1.0.1",
+    "version": "17.0.1.0.0",
     "category": "e-commerce",
     "website": "https://github.com/shopinvader/odoo-shopinvader",
     "author": "Akretion",
