53784a91b896a74ebbe039dbc7a8c80a78bb49eb
Daniel Sadamo <daniel.sadamo@kmee.com.br>
2025-07-25 18:38:26 -0300

    [MIG] shopinvader_product_template_tags: migrate to v17.0.1.0.0 with installable=False

--- shopinvader_product_template_tags/__manifest__.py
+++ shopinvader_product_template_tags/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Shopvinvader Product Template Tags",
     "summary": "Index Product Template Tags in Shopinvader",
-    "version": "14.0.1.0.0",
+    "version": "17.0.1.0.0",
     "category": "e-Commerce",
     "website": "https://github.com/shopinvader/odoo-shopinvader",
     "author": "Camptocamp",
