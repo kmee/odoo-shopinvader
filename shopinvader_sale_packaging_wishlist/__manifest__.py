3db29cb2ecba27beb3164548ff768c562e8639ec
Daniel Sadamo <daniel.sadamo@kmee.com.br>
2025-07-25 18:38:25 -0300

    [MIG] shopinvader_sale_packaging_wishlist: migrate to v17.0.1.0.0 with installable=False

--- shopinvader_sale_packaging_wishlist/__manifest__.py
+++ shopinvader_sale_packaging_wishlist/__manifest__.py
@@ -6,7 +6,7 @@
 {
     "name": "Shopinvader Wishlist Packaging",
     "summary": """Add packaging information to wishlists""",
-    "version": "14.0.1.0.0",
+    "version": "17.0.1.0.0",
     "license": "AGPL-3",
     "author": "Camptocamp,Odoo Community Association (OCA)",
     "website": "https://github.com/shopinvader/odoo-shopinvader",
