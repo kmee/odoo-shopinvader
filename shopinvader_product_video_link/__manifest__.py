54b218af17e0fc5a39b80d26a58029fbaba364eb
Daniel Sadamo <daniel.sadamo@kmee.com.br>
2025-07-25 18:38:26 -0300

    [MIG] shopinvader_product_video_link: migrate to v17.0.1.0.0 with installable=False

--- shopinvader_product_video_link/__manifest__.py
+++ shopinvader_product_video_link/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Shopinvader Product Video Link",
     "summary": "Add video on your Shopinvader website",
-    "version": "14.0.1.0.2",
+    "version": "17.0.1.0.0",
     "category": "Shopinvader",
     "website": "https://github.com/shopinvader/odoo-shopinvader",
     "author": " Akretion",
