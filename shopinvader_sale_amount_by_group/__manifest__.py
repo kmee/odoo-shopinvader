f81e4f0cf7bc482bd52118bb3f2274e27266edf0
Daniel Sadamo <daniel.sadamo@kmee.com.br>
2025-07-25 18:38:27 -0300

    [MIG] shopinvader_sale_amount_by_group: migrate to v17.0.1.0.0 with installable=False

--- shopinvader_sale_amount_by_group/__manifest__.py
+++ shopinvader_sale_amount_by_group/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Shopinvader Sale Amount by Tax",
     "summary": "Expose the amount by tax to shopinvader",
-    "version": "14.0.1.0.0",
+    "version": "17.0.1.0.0",
     "license": "AGPL-3",
     "author": "Camptocamp SA",
     "website": "https://github.com/shopinvader/odoo-shopinvader",
