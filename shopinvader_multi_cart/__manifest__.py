4269b666db90fdb7865197a9b34aa1452e5d044c
Daniel Sadamo <daniel.sadamo@kmee.com.br>
2025-07-25 18:38:26 -0300

    [MIG] shopinvader_multi_cart: migrate to v17.0.1.0.0 with installable=False

--- shopinvader_multi_cart/__manifest__.py
+++ shopinvader_multi_cart/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Shopinvader Multi-Cart",
     "summary": "Manage multiple carts in Shopinvader",
-    "version": "14.0.1.1.1",
+    "version": "17.0.1.0.0",
     "license": "AGPL-3",
     "author": "Camptocamp SA",
     "website": "https://github.com/shopinvader/odoo-shopinvader",
