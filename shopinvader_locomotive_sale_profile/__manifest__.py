c63a7aeb5a1955b9bdea02e06db8d3ddf9211f7f
Daniel Sadamo <daniel.sadamo@kmee.com.br>
2025-07-25 18:38:25 -0300

    [MIG] shopinvader_locomotive_sale_profile: migrate to v17.0.1.0.0 with installable=False

--- shopinvader_locomotive_sale_profile/__manifest__.py
+++ shopinvader_locomotive_sale_profile/__manifest__.py
@@ -7,7 +7,7 @@
 {
     "name": "Shopinvader Locomotive Sale Profile",
     "summary": "Synchronize the sale profile info to customer record on Locomotive",
-    "version": "14.0.1.0.1",
+    "version": "17.0.1.0.0",
     "category": "e-commerce",
     "website": "https://github.com/shopinvader/odoo-shopinvader",
     "author": "Akretion,Camptocamp",
