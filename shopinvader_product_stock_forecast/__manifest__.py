0cf1f2ff75fbb96d2a768dfdebf17b8c205bcc47
Daniel Sadamo <daniel.sadamo@kmee.com.br>
2025-07-25 18:38:27 -0300

    [MIG] shopinvader_product_stock_forecast: migrate to v17.0.1.0.0 with installable=False

--- shopinvader_product_stock_forecast/__manifest__.py
+++ shopinvader_product_stock_forecast/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Shopinvader Product Stock Forecast",
     "summary": "Export Stock Forecast data along with product stocks.",
-    "version": "14.0.1.0.0",
+    "version": "17.0.1.0.0",
     "author": "Camptocamp SA",
     "maintainers": ["ivantodorovich"],
     "website": "https://github.com/shopinvader/odoo-shopinvader",
