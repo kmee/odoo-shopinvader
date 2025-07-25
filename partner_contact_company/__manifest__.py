79012c0ccb533cd3f7131b39813ddbc05aab429d
Daniel Sadamo <daniel.sadamo@kmee.com.br>
2025-07-25 18:38:26 -0300

    [MIG] partner_contact_company: migrate to v17.0.1.0.0 with installable=False

--- partner_contact_company/__manifest__.py
+++ partner_contact_company/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Partner Company",
-    "version": "10.0.1.0.0",
+    "version": "17.0.1.0.0",
     "author": "Akretion",
     "website": "https://github.com/shopinvader/odoo-shopinvader",
     "license": "AGPL-3",
