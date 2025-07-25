abbebc686df3097fcf851937b49fab5855495318
Daniel Sadamo <daniel.sadamo@kmee.com.br>
2025-07-25 18:38:25 -0300

    [MIG] shopinvader_promotion_rule: migrate to v17.0.1.0.0 with installable=False

--- shopinvader_promotion_rule/__manifest__.py
+++ shopinvader_promotion_rule/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Shopvinvader Promotion Rule",
     "summary": "Module to manage Promotion Rule with shopinvader",
-    "version": "10.0.1.0.0",
+    "version": "17.0.1.0.0",
     "category": "Sale",
     "website": "https://github.com/shopinvader/odoo-shopinvader",
     "author": "Akretion, " "ACSONE SA / NV",
