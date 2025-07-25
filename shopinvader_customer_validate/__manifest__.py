fa34baea1edebcd747653b54013557f9f01de6ba
Daniel Sadamo <daniel.sadamo@kmee.com.br>
2025-07-25 18:38:25 -0300

    [MIG] shopinvader_customer_validate: migrate to v17.0.1.0.0 with installable=False

--- shopinvader_customer_validate/__manifest__.py
+++ shopinvader_customer_validate/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Shopinvader Customer Validation",
     "summary": """Provide configuration and machinery to validate customers.""",
-    "version": "14.0.1.3.0",
+    "version": "17.0.1.0.0",
     "license": "AGPL-3",
     "author": "Camptocamp,Odoo Community Association (OCA)",
     "website": "https://github.com/shopinvader/odoo-shopinvader",
