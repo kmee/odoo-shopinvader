1507013ffa154894d291575ef678386d4d1046b5
Daniel Sadamo <daniel.sadamo@kmee.com.br>
2025-07-25 18:38:26 -0300

    [MIG] shopinvader_sale_report: migrate to v17.0.1.0.0 with installable=False

--- shopinvader_sale_report/__manifest__.py
+++ shopinvader_sale_report/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "ACSONE SA/NV",
     "website": "https://github.com/shopinvader/odoo-shopinvader",
     "category": "e-commerce",
-    "version": "10.0.1.0.0",
+    "version": "17.0.1.0.0",
     "license": "AGPL-3",
     "depends": ["sale", "shopinvader"],
     "installable": False,
