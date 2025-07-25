3d618406457eafb7946f1e59e35828808527aa03
Daniel Sadamo <daniel.sadamo@kmee.com.br>
2025-07-25 18:38:25 -0300

    [MIG] shopinvader_restapi: migrate to v17.0.1.0.0 with installable=False

--- shopinvader_restapi/__manifest__.py
+++ shopinvader_restapi/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Shopinvader",
     "summary": "Shopinvader",
-    "version": "16.0.1.1.3",
+    "version": "17.0.1.0.0",
     "category": "e-commerce",
     "website": "https://github.com/shopinvader/odoo-shopinvader",
     "author": "Akretion,ACSONE SA/NV",
