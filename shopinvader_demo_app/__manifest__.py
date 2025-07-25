31a4e3fe201e60500babefad360207088736c76a
Daniel Sadamo <daniel.sadamo@kmee.com.br>
2025-07-25 18:38:26 -0300

    [MIG] shopinvader_demo_app: migrate to v17.0.1.0.0 with installable=False

--- shopinvader_demo_app/__manifest__.py
+++ shopinvader_demo_app/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     "name": "Shopinvader Demo App",
-    "version": "12.0.2.0.4",
+    "version": "17.0.1.0.0",
     "author": "ACSONE SA/NV",
     "website": "https://github.com/shopinvader/odoo-shopinvader",
     "license": "AGPL-3",
