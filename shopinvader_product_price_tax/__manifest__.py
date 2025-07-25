ba6db18711b19868ad3f38b8affdb0ea4ecfd886
Daniel Sadamo <daniel.sadamo@kmee.com.br>
2025-07-25 18:38:26 -0300

    [MIG] shopinvader_product_price_tax: migrate to v17.0.1.0.0 with installable=False

--- shopinvader_product_price_tax/__manifest__.py
+++ shopinvader_product_price_tax/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Shopinvader Product Price Tax",
     "summary": "Exposes product prices with and without taxes",
-    "version": "14.0.1.2.0",
+    "version": "17.0.1.0.0",
     "category": "e-commerce",
     "website": "https://github.com/shopinvader/odoo-shopinvader",
     "author": "Camptocamp SA",
