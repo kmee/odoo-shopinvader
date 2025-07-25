835e16e7150bae8e13ce125257174300c0570f1a
Daniel Sadamo <daniel.sadamo@kmee.com.br>
2025-07-25 18:38:26 -0300

    [MIG] shopinvader_locomotive: migrate to v17.0.1.0.0 with installable=False

--- shopinvader_locomotive/__manifest__.py
+++ shopinvader_locomotive/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Shopinvader Locomotive CMS Connector",
-    "version": "14.0.2.1.1",
+    "version": "17.0.1.0.0",
     "category": "Connector",
     "summary": "Manage communications between Shopinvader and Locomotive CMS",
     "author": "Akretion",
