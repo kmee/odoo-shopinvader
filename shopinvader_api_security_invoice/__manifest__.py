03f1ea6d9dbd688903a7ecec9af98dd66db04c1b
Daniel Sadamo <daniel.sadamo@kmee.com.br>
2025-07-25 18:38:26 -0300

    [MIG] shopinvader_api_security_invoice: migrate to v17.0.1.0.0 with installable=False

--- shopinvader_api_security_invoice/__manifest__.py
+++ shopinvader_api_security_invoice/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Shopinvader Api Security invoice",
     "summary": "Add security rule to expose invoices",
-    "version": "16.0.1.0.1",
+    "version": "17.0.1.0.0",
     "development_status": "Alpha",
     "category": "Uncategorized",
     "website": "https://github.com/shopinvader/odoo-shopinvader",
